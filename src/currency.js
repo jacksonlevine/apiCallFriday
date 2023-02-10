201dd3ea7bfe9de37e3f972c32de96db44c9b457
jacksonlevine <jack@netbug.us>
2023-02-10 08:47:37 -0800

    add static method to fetch supported codes

--- src/currency.js
+++ src/currency.js
@@ -1,3 +1,7 @@
 export class Currency {
-  
+  static async getSupportedCodes() {
+    return fetch(`https://v6.exchangerate-api.com/v6/${process.env.API_KEY}/codes`).then(
+      (response)=>{return response;},
+      (error)=>{return error;});
+  }
 }
